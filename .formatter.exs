39f078978b16ed21248150d22a5908d47bbe79ad
Eduardo Barreto Alexandre <blibs@blobs.com>
2026-03-05 21:01:47 -0300

    fix: Fixes .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -1,18 +1,23 @@
+locals_without_parens = [
+  attribute: 1,
+  attribute: 2,
+  argument: 1,
+  argument: 2,
+  calculation: 1,
+  calculation: 2,
+  action: 1,
+  action: 2,
+  title: 1,
+  description: 1,
+  code_sample: 1
+]
+
 [
   import_deps: [:ash, :spark],
   plugins: [Spark.Formatter],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  locals_without_parens: [
-    attribute: 1,
-    attribute: 2,
-    argument: 1,
-    argument: 2,
-    calculation: 1,
-    calculation: 2,
-    action: 1,
-    action: 2,
-    title: 1,
-    description: 1,
-    code_sample: 1
+  locals_without_parens: locals_without_parens,
+  export: [
+    locals_without_parens: locals_without_parens
   ]
 ]
